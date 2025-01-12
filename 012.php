dd8ae1dc100e2602f89ff11e9fe3ea5bbb7bbbb2
tsunoda-s-09 <tsunoda-s@eslo.jp>
2025-01-12 11:51:00 +0900

    言語リファレンス > breakとcontinue

---
+++ 012.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * @link https://www.phpexam.jp/archives/3210
+ * @link https://www.php.net/manual/ja/control-structures.break.php
+ * @link https://www.php.net/manual/ja/control-structures.continue.php
+ */
