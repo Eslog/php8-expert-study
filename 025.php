6f5e147c48ac1bda6204b564bf75dc88ec97e6ff
tsunoda-s-09 <tsunoda-s@eslo.jp>
2025-01-12 15:16:17 +0900

    言語リファレンス オブジェクト インターフェイス

--- 025.php
+++ 025.php
@@ -1,5 +1,5 @@
 <?php
 /**
- * @link https://www.phpexam.jp/archives/3645
- * @link https://www.php.net/manual/ja/language.oop5.abstract.php
+ * @link https://www.phpexam.jp/archives/3649
+ * @link https://www.php.net/manual/ja/language.oop5.interfaces.php
  */
